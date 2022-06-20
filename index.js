8bb4d8ef4a588388e84ef71767bec1e8265ec4cf
Skylar Stevens <skylykat69@gmail.com>
2022-06-20 13:51:15 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const { MongoClient } = require('mongodb');
 const { createClient } = require('redis');
+const fetch = require('sync-fetch')
 const mc = require('minecraft-protocol');
 const fs = require('fs');
 const { nanoid } = require('nanoid');
 
 let e = s => Buffer.from(s).toString('base64');
 
-const mongo = new MongoClient('mongodb://admin:securePassword123@192.168.1.119:27017/?authMechanism=DEFAULT');
 const redis = new createClient({
 	host: '127.0.0.1',
 	port: 6379
@@ -33,7 +32,6 @@ let beforePing = (res, client, cb) => {
 const EX = 300;
 
 (async () => {
-	await mongo.connect();
 	await redis.connect();
 	console.log('Figura auth server is online.');
 })();
@@ -41,30 +39,22 @@ const EX = 300;
 const server = mc.createServer({
 	version: false,
 	motd: 'figura auth server',
-	port: 25565,
+	host: '127.0.0.1',
+	port: 25566,
 	beforePing: beforePing,
-	onlineMode: true
+	'online-mode': false
 });
 
+const uuidF = i=>i.substr(0,8)+"-"+i.substr(8,4)+"-"+i.substr(12,4)+"-"+i.substr(16,4)+"-"+i.substr(20);
+
 server.on('login', async (client) => {
-	console.log(`Getting auth token for ${client.username}`);
+	const uuid = uuidF(fetch(`https://api.mojang.com/users/profiles/minecraft/${client.username}`).json().id);
+	console.log(`Getting auth token for ${client.username} [${uuid}]`);
 
-	const uuid = client.uuid.replace(/-/g, '');
-	const user = await mongo.db('figura').collection('users').findOne({ uuid });
-
-	let token = null;
-
-	if (user?.banned) return client.end(JSON.stringify({ type: 'banned', reason: user.banned }));
-
-	token = await redis.get(uuid);
-	if (token) console.log(`Authenticated ${client.username} with ${token}, closing connection. (cached token)`);
-	if (token) return client.end(JSON.stringify({ type: 'auth', token: token }));
-
-	token = nanoid(10);
-	await redis.set(uuid, token, { EX });
+	let token = nanoid(20);
 	await redis.set(token, uuid, { EX });
 
-	console.log(`Authenticated ${client.username} with ${token}, closing connection.`);
+	console.log(`Authenticated ${client.username} with ${token}.`);
 
-	client.end(JSON.stringify({ type: 'auth', token: token }));
+	client.end(token);
 });
